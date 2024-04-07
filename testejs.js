3f4d73bb1cf1281635dcbbdff47473cd62b0b7fa
CesarCouto <cesarrcouto@outlook.com>
2024-04-07 02:18:51 -0300

    primiro commit

---
+++ testejs.js
@@ -0,0 +1,36 @@
+function mostra_camada_1(){
+    document.getElementById("camada_1").style.visibility="visible";
+    document.getElementById("camada_2").style.visibility="hidden";
+    document.getElementById("camada_3").style.visibility="hidden";
+}
+
+function mostra_camada_2(){
+    document.getElementById("camada_1").style.visibility="hidden";
+    document.getElementById("camada_2").style.visibility="visible";
+    document.getElementById("camada_3").style.visibility="hidden";
+}
+
+function mostra_camada_3(){
+    document.getElementById("camada_1").style.visibility="hidden";
+    document.getElementById("camada_2").style.visibility="hidden";
+    document.getElementById("camada_3").style.visibility="visible";
+}
+
+function some_tudo(){
+    document.getElementById("camada_1").style.visibility="hidden";
+    document.getElementById("camada_2").style.visibility="hidden";
+    document.getElementById("camada_3").style.visibility="hidden";
+}
+
+/*
+function some_camada_1(){
+    document.getElementById("camada_1").style.visibility="hidden";
+}
+
+function some_camada_2(){
+    document.getElementById("camada_2").style.visibility="hidden";
+}
+
+function some_camada_3(){
+    document.getElementById("camada_3").style.visibility="hidden";
+} */
